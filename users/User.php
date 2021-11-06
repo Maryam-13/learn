317b7d2ff740cdce08e5e29fa5a3cb8ec70d858d
Maryam-13 <mary_am79@mail.ru>
2021-11-06 10:15:19 +0300

    Create User.php

---
+++ users/User.php
@@ -0,0 +1,23 @@
+<?php
+
+if (!empty ($_REQUEST["name"])){
+
+class User{    
+public $name, $pass, $pass_conf, $mail;
+}
+    
+$user = new User();   
+    
+    
+$user->name = $_REQUEST["name"];
+$user->pass = $_REQUEST["password"];
+$user->pass_conf = $_REQUEST['password-conf'];
+$user->mail = $_REQUEST["e-mail"];
+    
+    
+}
+
+
+
+?>
+
