785de477d5e046a79e9a532009bae1e8dd9e8627
Maryam-13 <mary_am79@mail.ru>
2021-11-07 11:10:10 +0300

    Update User.php

--- users/User.php
+++ users/User.php
@@ -6,18 +6,13 @@ class User{
 public $name, $pass, $pass_conf, $mail;
 }
     
-$user = new User();   
+//$user = new User();   
     
-    
-$user->name = $_REQUEST["name"];
-$user->pass = $_REQUEST["password"];
-$user->pass_conf = $_REQUEST['password-conf'];
-$user->mail = $_REQUEST["e-mail"];
-    
-    
+//$user->name = $_REQUEST["name"];
+//$user->pass = $_REQUEST["password"];
+/*$user->pass_conf = $_REQUEST['password-conf'];
+$user->mail = $_REQUEST["e-mail"];*/
+ 
 }
-
-
-
 ?>
 
