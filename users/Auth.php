501057f751ee61948ef21da7bbaf7d4d8a54a150
Maryam-13 <mary_am79@mail.ru>
2021-11-16 11:29:12 +0300

    Create Auth.php

---
+++ users/Auth.php
@@ -0,0 +1,14 @@
+<?php
+class Auth {
+static function add_user_to_session($user) { // Статическая функция - не требуется создавать экземпляр класса, чтобы вызвать функцию.
+if (!empty($user)){
+$users = $_SESSION["users"];
+$users[]= $user;
+$_SESSION["users"] = $users;
+}
+return $users;
+}
+}
+?>
+
+
