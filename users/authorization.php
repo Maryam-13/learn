1d206da500856b81020b6877d5561be8c205fb81
Maryam-13 <mary_am79@mail.ru>
2021-11-16 11:29:09 +0300

    Update authorization.php

--- users/authorization.php
+++ users/authorization.php
@@ -1,48 +1,20 @@
 <?php
-require_once("User.php");
-$user = new User();
-
-require_once("Database.php");
-$database = new Database;
-
 session_start();
 
-//if (!empty ($_REQUEST[$user->name])){
-$connection =  $database->connect();  
-mysqli_set_charset($connection, "utf8mb4"); 
-    
-$user->name = $_REQUEST["name"];
-$user->pass = $_REQUEST["password"];
+require_once("Auth.php");
+require_once("User.php");
 
-$users = [];  
- 
-     
-if (!empty($user->name)) { 
-      if (!empty($user->pass)){
-          
-          $pass = MD5($user->pass);
-          
-          $resource = $database->get_query($connection,"SELECT `id` FROM `users` WHERE name LIKE '$user->name' and password LIKE '$pass' ");
-                  
-          $row = mysqli_fetch_assoc($resource);
-         
-                  
-          if (!empty($row)){
-              
-          $users = $_SESSION["users"];
-              
-          $users[]= $row;
-          
-          $_SESSION["users"] = $users; 
-             
-          }
-              
-          }
-         
-      }
+$user = new User(); // Шаг 1. Получить данны пользователя
+$users = [];
 
-//}
+if ($user->name_and_pass_exists()) { // Шаг 2. Проверить пришли данные
+$row = $user->load(); // Шаг 3. Получить пользователя из БД
 
+   
+$users = Auth::add_user_to_session($row); // Шаг 4. Добавить пользователя в сессию. // Вызов статической функции.
+};
+
+//header("Location: authorization.php");
 ?>
 
 <!doctype html>
