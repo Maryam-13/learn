b88c740c583f29eb3887fcab3871da2e6ef91f34
Maryam-13 <mary_am79@mail.ru>
2021-11-28 09:36:40 +0300

    Update authorization.php

--- users/authorization.php
+++ users/authorization.php
@@ -1,17 +1,18 @@
 <?php
-session_start();
 
 require_once("Auth.php");
 require_once("User.php");
 
 $user = new User(); // Шаг 1. Получить данные пользователя
-$users = [];
+//$users = [];
 
 if ($user->name_and_pass_exists()) { // Шаг 2. Проверить пришли данные
 $row = $user->load(); // Шаг 3. Получить пользователя из БД
   
-$users = Auth::add_user_to_session($row); // Шаг 4. Добавить пользователя в сессию. // Вызов статической функции.
-    
+//$users = Auth::add_user_to_session($row); // Шаг 4. Добавить пользователя в сессию. // Вызов статической функции.
+
+    Auth::add_user_to_session($row["id"]);
+    $name_id = $_SESSION['id'];
 };
 //header("Location: authorization.php");
 ?>
@@ -30,21 +31,23 @@ $users = Auth::add_user_to_session($row); // Шаг 4. Добавить поль
 <form class = "form" method="post">
    <input class = "box" type="text" name="name" placeholder="имя">
    <input class = "box" type="password" name="password" placeholder="пароль">
-    
-   <input class = "button" type="submit" value="Войти">       
+   <input class = "button" type="submit" value="Войти">
+    <a href="posts.php?name_id=<?php echo $name_id;?>" class="link">Войти   </a>   
    <a href = "register.php" class="link"> <input class = "button" type="button" value="Регистрация"> </a>
- 
-</form>
+ </form>
     
      <div>
     <?php
     require_once("User.php");
     $user = new User();
          
-    if (!empty($user->name)) {     
-    echo "<pre>";
-    print_r($users);
-    }
+    //if (!empty($user->name)) {     
+    //echo "<pre>";
+    //print_r($users);
+    //}
+         
+      // echo $user->name;  //выводит имя пользователя
+       echo $name_id;  
     ?>
     </div>
 </body>
