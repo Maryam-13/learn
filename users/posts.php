98b76c598cf97acd5c586327fb17f6b71cec7d54
Maryam-13 <mary_am79@mail.ru>
2021-11-28 09:36:59 +0300

    Update posts.php

--- users/posts.php
+++ users/posts.php
@@ -1,26 +1,30 @@
 <?php
-session_start();
+//session_start();
+
 
 require_once("Auth.php");
 require_once("User.php");
 require_once("Post.php");
 
+$user = new User();
 $post = new Post(); // Получить данные пользователя
 $posts = [];
 $titles = [];
 $dates = [];
 
-$title = $post->output_t();
+$name_id = $_REQUEST["name_id"];
+
+$title = $post->output_t($name_id);
 while ($row = mysqli_fetch_assoc($title)){
     $titles[] = $row;
 }
 
-$note = $post->output();
+$note = $post->output($name_id);
 while ($row = mysqli_fetch_assoc($note)){
     $posts[] = $row;
 }
 
-$date = $post->output_d();
+$date = $post->output_d($name_id);
 while ($row = mysqli_fetch_assoc($date)){
     $dates[] = $row;
 }
@@ -41,14 +45,15 @@ while ($row = mysqli_fetch_assoc($date)){
     $i = 0;
     foreach($posts as $post){
     print_r('<div class="title">'.$titles[$i]["title"].'</div>');
-    print_r(mb_substr($posts[$i]["content"], 0, 150));
+    print_r(mb_substr($posts[$i]["content"], 0, 150)); ?>
+    <a href="post1.php?i=<?php echo $i; ?>&name_id=<?php echo $name_id; ?>" class="data">Подробнее</a><?php
     print_r('<div class="data">'.$dates[$i]["date"].'</div>');
     $i = $i + 1;
    }
     ?>
     </div>
     <div class="data">
-    <a href="create.php" >Создать</a>
+    <a href="create.php?name_id=<?php echo $name_id; ?>" >Создать</a>
     </div>
 </body>
 </html>
