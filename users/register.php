84bbd6b15a5beb7fd0b744679cec64bacdcda407
Maryam-13 <mary_am79@mail.ru>
2021-12-07 16:39:23 +0300

    Update register.php

--- users/register.php
+++ users/register.php
@@ -12,22 +12,3 @@ if ($user->name_and_pass_exists()) {
     }
 }
 ?>
-
-<!doctype html>
-<html>
-<head>
-    <title>Регистрация</title>
-    <link href="style.css" rel="stylesheet">
-</head>
-<body>
-<div class="text"> Мой блог
-</div>
-<form class="form" method="post">
-    <input class="box" type="text" name="name" placeholder="имя">
-    <input class="box" type="password" name="password" placeholder="пароль">
-    <input class="box" type="password" name="password-conf" placeholder="подтверждение пароля">
-    <input class="box" type="text" name="e-mail" placeholder="e-mail">
-    <input class="button" type="submit" value="Регистрация">
-</form>
-</body>
-</html>
