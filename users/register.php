39bc07b25c68e38d6e8a13939a2c69854b75df24
Maryam-13 <mary_am79@mail.ru>
2021-12-11 11:44:28 +0300

    Update register.php

--- users/register.php
+++ users/register.php
@@ -8,7 +8,8 @@ $database = new Database;
 if ($user->name_and_pass_exists()) {
     if ($user->pass == $user->pass_conf) {
         $user->user_insert();
-        header("Location: register.php");
+      //  header("Location: register.php");
+      print_r($user) ;
     }
 }
 ?>
