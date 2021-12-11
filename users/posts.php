5f1e90f06577f065923b7031091221d1499215a5
Maryam-13 <mary_am79@mail.ru>
2021-12-11 11:44:23 +0300

    Update posts.php

--- users/posts.php
+++ users/posts.php
@@ -15,32 +15,10 @@ $name_id = $_SESSION['id'];
 $posts = $post->output_all($name_id);
 $post = $database->get_array($posts);
 
-?>
+//print_r($post);
+echo json_encode($post);
 
-<!doctype html>
-<html>
-<head>
-    <title>Посты</title>
-    <link href="style.css" rel="stylesheet">
-</head>
-<body>
-<div class="text"> Мой блог
-</div>
-<div class="content">
-    <?php
-      foreach ($posts as $post) { 
-        print_r('<div class="title">' . $post["title"] . '</div>');
-        print_r('<div class="content">' . mb_substr($post["content"], 0, 150) . '</div>'); ?>
-        <a href="post.php?i=<?php echo $post["id"]; ?>&name_id=<?php echo $name_id; ?>" class="data">Подробнее</a><?php
-        print_r('<div class="data">' . $post["date"] . '</div>');
-        }
-    ?>
-</div>
-<div class="data">
-    <a href="create.php?name_id=<?php echo $name_id; ?>">Создать</a>
-</div>
-</body>
-</html>
+?>
 
 
     
