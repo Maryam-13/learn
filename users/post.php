6a563854cac1621656bbf049b9ee01fd36e555f0
Maryam-13 <mary_am79@mail.ru>
2021-12-19 23:09:30 +0300

    Update post.php

--- users/post.php
+++ users/post.php
@@ -8,41 +8,19 @@ $post = new Post();
 $database = new Database;
 $posts = [];
 
-$i = $_REQUEST["i"];
-$name_id = $_REQUEST["name_id"];
+$i = 10;
 
-$posts = $post->output_one($name_id, $i);
+
+//$name_id = $_REQUEST["name_id"];
+
+//$posts = $post->output_one($name_id, $i);
+$posts = $post->output_one($i);
 $note = $database->get_array($posts);
+
+echo json_encode($note, JSON_UNESCAPED_UNICODE);
 
 if (!empty($_REQUEST["delete_post"])) {
     $post->post_delete($i);
     header("Location: posts.php");
 }
 ?>
-
-<!doctype html>
-<html>
-<head>
-    <title>Пост</title>
-    <link href="style.css" rel="stylesheet">
-</head>
-<body>
-<div class="text"> Мой блог
-</div>
-<div class="content">
-    <?php
-        print_r('<div class="title">' . $note["title"] . '</div>');
-        print_r($note["content"]);
-        print_r('<div class="data">' . $note["date"] . '</div>');
-    ?>
-</div>
-<div class="data">
-    <a href="change.php?i=<?php echo $i; ?>&name_id=<?php echo $name_id; ?>">Изменить</a>
-    <form method="post">
-        <input type="submit" name="delete_post" value="Удалить">
-    </form>
-</div>
-</body>
-</html>
-
-    
