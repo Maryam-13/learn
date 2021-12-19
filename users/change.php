1ef25bdff7d50deff595e461d130b7c42b02f058
Maryam-13 <mary_am79@mail.ru>
2021-12-19 23:09:22 +0300

    Update change.php

--- users/change.php
+++ users/change.php
@@ -9,36 +9,18 @@ $database = new Database;
 
 $posts = [];
 
-$i = $_REQUEST["i"];
-$name_id = $_REQUEST["name_id"];
+$i = 10;//$_REQUEST["i"];
+//$name_id = $_REQUEST["name_id"];
 
-$posts = $post->output_one($name_id,$i);
+$posts = $post->output_one($i);
 $note = $database->get_array($posts);
+echo json_encode($note, JSON_UNESCAPED_UNICODE);
 
 if (!empty($_REQUEST["change"])) {
     $post->post_change($i);
-    header("Location: posts.php");
+    header("Location: posts.html");
 }
 ?>
-
-<!doctype html>
-<html>
-<head>
-    <title>Изменить</title>
-    <link href="style.css" rel="stylesheet">
-</head>
-<body>
-<div class="text"> Мой блог
-</div>
-<div class="content">
-    <form class="form change" method="post">
-        <input class='box change' type='text' name='title' value='<?php echo $note["title"] ?>'>
-        <textarea class="textarea" type='text' name='content' ><?php echo $note["content"] ?></textarea>
-        <input class="button" type="submit" name="change" value="Сохранить">
-    </form>
-</div>
-</body>
-</html>
 
 
     
