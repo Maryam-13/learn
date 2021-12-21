55a3d9548bdfbe4bfc8492ebb4a044cba078672f
Maryam-13 <mary_am79@mail.ru>
2021-12-21 23:03:21 +0300

    Update create.js

--- users/create.js
+++ users/create.js
@@ -1,28 +1,38 @@
-        let searchParams = new URLSearchParams(window.location.search);
+let searchParams = new URLSearchParams(window.location.search);
+let name_id = String(searchParams);
+let i = name_id.slice(0,2);
 
-       let name_id = String(searchParams);
-       let i = name_id.slice(8);
-       
-       let heading = document.createElement("div");
-        document.body.appendChild(heading);
 
-        heading.appendChild(document.createTextNode("Мой блог"));
-        heading.className = "text";
+function create_heading() {
+    let heading = document.createElement("div");
+    document.body.appendChild(heading);
+    heading.appendChild(document.createTextNode("Мой блог"));
+    heading.className = "text";
+}
+create_heading();
 
-        let form = document.createElement("form");
-        document.body.appendChild(form);
-        form.className = "form change";
+function create_form() {
+    let form = document.createElement("form");
+    document.body.appendChild(form);
+    form.className = "form change";
 
+    function create_title() {
         let title = document.createElement("input");
         form.appendChild(title);
         title.className = "box change";
         title.id = "title";
+    }
+    create_title();
 
+    function create_content() {
         let content = document.createElement("textarea");
         form.appendChild(content);
         content.className = "textarea";
         content.id = "content";
+    }
+    create_content();
 
+    function create_button() {
         let button = document.createElement("button");
         form.appendChild(button);
         button.className = "button";
@@ -30,16 +40,20 @@
         button.appendChild(text);
         button.onclick = create;
 
-        function create(event){
+        function create(event) {
             event.preventDefault();
-             let data = new FormData();
-             data.append("title", title.value);
-             data.append("content", content.value);
-             data.append("name_id", i);
-               
-             fetch("create.php", {method: "post", body: data}) 
-             .then(response => response.text())
-             .then(result =>{            
-             window.location.href = "posts.html";
-             })
+            let data = new FormData();
+            data.append("title", title.value);
+            data.append("content", content.value);
+            data.append("name_id", i);
+
+            fetch("create.php", { method: "post", body: data })
+                .then(response => response.text())
+                .then(result => {
+                    window.location.href = "posts.html";
+                })
         }
+    }
+    create_button();
+}
+create_form();
