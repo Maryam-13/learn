f1f7b9175a7acfb4591529083478d2ba0cb2e81e
Maryam-13 <mary_am79@mail.ru>
2022-01-11 23:31:36 +0300

    Update authorization.js

--- users/js/authorization.js
+++ users/js/authorization.js
@@ -1,26 +1,28 @@
-page.create_heading();
+(function(){
+    page.create_heading();
 
-let form = page.create_form(document.body, "form");
-page.create_name();
-page.create_pass();
-page.create_button(form, "Войти", enter);
-page.create_button(form, "Регистрация", register);
+    form = page.create_form(document.body, "form");
+    page.create_name();
+    page.create_pass();
+    page.create_button(form, "Войти", enter);
+    page.create_button(form, "Регистрация", register);
 
-function enter(event) {
-    event.preventDefault();
-    event.cancelBubble = true;
-    let data = new FormData();
-    data.append("user_name", user_name.value);
-    data.append("password", password.value);
+    function enter(event) {
+        event.preventDefault();
+        event.cancelBubble = true;
+        let data = new FormData();
+        data.append("user_name", user_name.value);
+        data.append("password", password.value);
 
-    fetch("/users/authorization.php", { method: "post", body: data })
-        .then(response => response.text())
-        .then(result => {
-            window.location.href = "/users/posts.html";
-        })
-}
+        fetch("/users/authorization.php", { method: "post", body: data })
+            .then(response => response.text())
+            .then(result => {
+                window.location.href = "/users/posts.html";
+            })
+    }
 
-function register(event) {
-    event.preventDefault();
-    window.location.href = "/users/register.html";
-}
+    function register(event) {
+        event.preventDefault();
+        window.location.href = "/users/register.html";
+    }
+})();
