21f5f4b39a1e1fb394e12549cd51bfae3de8103a
Maryam-13 <mary_am79@mail.ru>
2022-01-11 23:31:52 +0300

    Update post.js

--- users/js/post.js
+++ users/js/post.js
@@ -1,44 +1,45 @@
+var blog = {};
+(function () {
 
-window.addEventListener('DOMContentLoaded', load);
+    blog.load_post_page = load;
 
-let searchParams = new URLSearchParams(window.location.search);
-let i = String(searchParams);
-let id = i.slice(0, 2);
+    page.create_heading();
+    let id = page.get_id();
+    create_link();
 
-page.create_heading();
-create_link();
+    function load() {
 
-function load() {
+        fetch("/users/post.php?id=" + id)
+            .then(response => response.json())
+            .then(on_load)
+    }
 
-    fetch("/users/post.php?id=" + id)
-        .then(response => response.json())
-        .then(on_load)
-}
+    function on_load(result) {
+        let post = result;
 
-function on_load(result){
-    let post = result;
+        page.create_title(post.title);
+        page.create_content(post.content);
+        page.create_date(post.date);
+        page.create_button(document.body, "Удалить", delete_post1);
+    }
 
-    page.create_title(post.title);
-    page.create_content(post.content);
-    page.create_date(post.date);
-    page.create_button(document.body, "Удалить", delete_post1);
-}
+    function delete_post1(event) {
+        let data = new FormData();
+        event.preventDefault();
+        data.append("delete_post", 1);
+        fetch("/users/post.php?id=" + id, { method: "post", body: data })
+            .then(response => response.text())
+            .then(result => {
+                window.location.href = "/users/posts.html";
+            })
+    }
 
-function delete_post1(event) {
-    let data = new FormData();
-    event.preventDefault();
-    data.append("delete_post", 1);
-    fetch("/users/post.php?id=" + id, { method: "post", body: data })
-        .then(response => response.text())
-        .then(result => {
-            window.location.href = "/users/posts.html";
-        })
-}
-
-function create_link() {
-    let word = document.createElement("a");
-    document.body.appendChild(word);
-    let linkText = document.createTextNode("Изменить");
-    word.appendChild(linkText);
-    word.href = '/users/change.html?' + id;
-}
+    function create_link() {
+        let word = document.createElement("a");
+        document.body.appendChild(word);
+        let linkText = document.createTextNode("Изменить");
+        word.appendChild(linkText);
+        word.href = '/users/change.html?' + id;
+    }
+})();
+window.addEventListener('DOMContentLoaded', blog.load_post_page);
