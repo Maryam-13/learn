a9f712cc4b00d60803f5ce63a89621571a6bf983
Maryam-13 <mary_am79@mail.ru>
2022-01-11 23:31:57 +0300

    Update posts.js

--- users/js/posts.js
+++ users/js/posts.js
@@ -1,47 +1,52 @@
-window.addEventListener('DOMContentLoaded', load);
+var blog = {};
+(function () {
 
-page.create_heading();
-page.create_button(document.body, "Создать", create);
+    blog.load_posts_page = load;
 
-let ob_name_id = {};
+    page.create_heading();
+    page.create_button(document.body, "Создать", create);
 
-function load() {
+    let ob_name_id = {};
 
-    fetch("/users/posts.php")
-        .then(response => response.text())
-        .then(on_load)
-}
+    function load() {
 
-function on_load(result){
-    let a = [];
-    for (let i = 0; i < result.length; i++) {
-        a[i] = result.slice(result.indexOf('{'), result.indexOf('}') + 1);
-        result = result.slice(result.indexOf('}') + 1);
+        fetch("/users/posts.php")
+            .then(response => response.text())
+            .then(on_load)
     }
 
-    for (let i = 0; i < a.length; i++) {
+    function on_load(result) {
+        let a = [];
+        for (let i = 0; i < result.length; i++) {
+            a[i] = result.slice(result.indexOf('{'), result.indexOf('}') + 1);
+            result = result.slice(result.indexOf('}') + 1);
+        }
 
-        let post = JSON.parse(a[i]);
+        for (let i = 0; i < a.length; i++) {
 
-        page.create_title(post.title);
-        page.create_content(post.content);
-        create_link();
-        page.create_date(post.date);
-        
-        ob_name_id = post.name_id;
+            let post = JSON.parse(a[i]);
+
+            page.create_title(post.title);
+            page.create_content(post.content);
+            create_link(post.id);
+            page.create_date(post.date);
+
+            ob_name_id = post.name_id;
+        }
     }
-}
 
-function create_link() {
-    let word = document.createElement("a");
-    document.body.appendChild(word);
-    let linkText = document.createTextNode("Подробнее...");
-    word.appendChild(linkText);
-    word.title = "Подробнее...";
-    let ob_id = post.id;
-    word.href = '/users/post.html?' + ob_id;
-}
+    function create_link(post_id) {
+        let word = document.createElement("a");
+        document.body.appendChild(word);
+        let linkText = document.createTextNode("Подробнее...");
+        word.appendChild(linkText);
+        word.title = "Подробнее...";
+        let ob_id = post_id;
+        word.href = '/users/post.html?' + ob_id;
+    }
 
-function create() {
-    window.location.href = "/users/create.html?" + ob_name_id;
-}
+    function create() {
+        window.location.href = "/users/create.html?" + ob_name_id;
+    }
+})();
+window.addEventListener('DOMContentLoaded', blog.load_posts_page);
