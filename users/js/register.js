f9fb9564227c87ac0cd743198a5723c8070bd1ea
Maryam-13 <mary_am79@mail.ru>
2022-01-17 00:16:56 +0300

    Update register.js

--- users/js/register.js
+++ users/js/register.js
@@ -1,26 +1,28 @@
 (function () {
-    page.create_heading();
+    page.create_heading;
     form = page.create_form(document.body, "form");
-    page.create_name();
-    page.create_pass();
-    create_pass_conf();
-    create_mail();
+    $(form).append(page.create_name);
+    $(form).append(page.create_pass);
+    //create_pass_conf();
+    $(form).append("<input class='box' id='password_conf' type='password'>")
+    //create_mail();
+    $(form).append("<input class='box' id='mail'>")
     page.create_button(form, "Регистрация", register);
 
-    function create_pass_conf() {
+        /*function create_pass_conf() {
         let password_conf = document.createElement("input");
         form.appendChild(password_conf);
         password_conf.className = "box";
         password_conf.id = "password_conf";
         password_conf.setAttribute("type", "password");
-    }
+    }*/
 
-    function create_mail() {
+    /*function create_mail() {
         let mail = document.createElement("input");
         form.appendChild(mail);
         mail.className = "box";
         mail.id = "mail";
-    }
+    }*/
 
     function register(event) {
         event.preventDefault();
