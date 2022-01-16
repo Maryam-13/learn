c43e9b41251e53e5a6e9038b8c2c3632bfe6aae4
Maryam-13 <mary_am79@mail.ru>
2022-01-17 00:16:45 +0300

    Update create.js

--- users/js/create.js
+++ users/js/create.js
@@ -1,30 +1,32 @@
 (function(){
-function get_name_id() {
-    let searchParams = new URLSearchParams(window.location.search);
-    let i = String(searchParams);
-    let name_id = i.slice(0, 2);
-    return name_id;
-}
 
-page.create_heading();
+let name_id = page.get_parameter();
+
+page.create_heading;
 form = page.create_form(document.body, "form change");
-create_title();
-create_content();
+//create_title();
+//create_content();
+$(form)
+.append('<input id="title" class="box change"></tinput>')
+
+$(form)
+.append('<textarea id="content" class="textarea"></textarea>')
+
 page.create_button(form, "Создать", create);
 
-function create_title() {
+/*function create_title() {
     let title = document.createElement("input");
     form.appendChild(title);
     title.className = "box change";
     title.id = "title";
-}
+}*/
 
-function create_content() {
+/*function create_content() {
     let content = document.createElement("textarea");
     form.appendChild(content);
     content.className = "textarea";
     content.id = "content";
-}
+}*/
 
 function create(event) {
     //event.cancelBubble = true;
@@ -34,7 +36,7 @@ function create(event) {
     data.append("content", content.value);
     //data.append("name_id", name_id);
 
-    fetch("/users/create.php?name_id=" + get_name_id(), { method: "post", body: data })
+    fetch("/users/create.php?name_id=" + name_id, { method: "post", body: data })
         .then(response => response.text())
         .then(result => {
             window.location.href = "/users/posts.html";
