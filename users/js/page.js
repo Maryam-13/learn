a4bd1dd72c2bea3fb82c3d5593fff2c355fae3d5
Maryam-13 <mary_am79@mail.ru>
2022-01-17 00:16:47 +0300

    Update page.js

--- users/js/page.js
+++ users/js/page.js
@@ -1,22 +1,26 @@
 let page = {};
 
 (function () {
-    page.create_heading = create_heading1;
+    //page.create_heading = create_heading1;
     page.create_form = create_form;
     page.create_title = create_title;
     page.create_content = create_content;
     page.create_date = create_date;
-    page.create_name = create_name;
-    page.create_pass = create_pass;
+    //page.create_name = create_name;
+    //page.create_pass = create_pass;
     page.create_button = create_button;
-    page.get_id = get_id;
+    page.get_parameter = get_parameter;
 
-    function create_heading1() {
+    page.create_heading = $(document.body).append('<div class="text">Мой блог</div>');
+    
+    page.create_name = $("<input class='box' id='user_name'>");
+    page.create_pass = $("<input class='box' id='password' type='password'>");
+    /*function create_heading1() {
         let heading = document.createElement("div");
         document.body.appendChild(heading);
         heading.appendChild(document.createTextNode("Мой блог"));
         heading.className = "text";
-    }
+    }*/
     function create_form(where, className) {
         let form = document.createElement("form");
         where.appendChild(form);
@@ -44,19 +48,19 @@ let page = {};
         date1.appendChild(text);
         date1.className = "data";
     }
-    function create_name() {
+    /*function create_name() {
         let user_name = document.createElement("input");
         form.appendChild(user_name);
         user_name.className = "box";
         user_name.id = "user_name";
-    }
-    function create_pass() {
+    }*/
+    /*function create_pass() {
         let password = document.createElement("input");
         form.appendChild(password);
         password.className = "box";
         password.id = "password";
         password.setAttribute("type", "password");
-    }
+    }*/
     function create_button(where, word, function_name) {
         let button = document.createElement("button");
         where.appendChild(button);
@@ -65,10 +69,9 @@ let page = {};
         button.appendChild(text);
         button.onclick = function_name;
     }
-    function get_id() {
+    function get_parameter() {
         let searchParams = new URLSearchParams(window.location.search);
-        let i = String(searchParams);
-        let id = i.slice(0, 2);
-        return id;
+        let params = String(searchParams);
+        return params.slice(0, 2);
     }
 })();
