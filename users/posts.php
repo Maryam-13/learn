5936745945187dcc4cd74bbd055db41edc8bdcdb
Maryam-13 <mary_am79@mail.ru>
2022-01-21 18:26:34 +0300

    Update posts.php

--- users/posts.php
+++ users/posts.php
@@ -11,13 +11,29 @@ $database = new Database;
 $posts = [];
 
 $name_id = $_SESSION['id'];
-//print_r($name_id);
+$change_date = $_REQUEST["change_date"];
+$press = $_REQUEST["press"];
 
-$posts = $post->output_all($name_id);
+//$now_date = date('Y-m-d');
 
-$result = [];
+if ($press == 0) {
 
-for ($i = 0; $i < $posts->num_rows; $i++){
-$result[] = $database->get_array($posts);
+    $posts = $post->output_all($name_id);
+
+    $result = [];
+
+    for ($i = 0; $i < $posts->num_rows; $i++) {
+        $result[] = $database->get_array($posts);
+    }
+    echo json_encode($result, JSON_UNESCAPED_UNICODE);
+} else {
+    
+    $posts = $post->output_from_date($name_id, $change_date);
+
+    $result = [];
+   
+    for ($i = 0; $i < $posts->num_rows; $i++) {
+        $result[] = $database->get_array($posts);
+    }
+    echo json_encode($result, JSON_UNESCAPED_UNICODE);
 }
-echo json_encode($result, JSON_UNESCAPED_UNICODE);
