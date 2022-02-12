a004b150378e67f281d1a999a5cbbc4230d5bd23
Maryam-13 <mary_am79@mail.ru>
2022-02-12 13:57:19 +0300

    Update show.blade.php

--- blog/resources/views/products/show.blade.php
+++ blog/resources/views/products/show.blade.php
@@ -2,41 +2,36 @@
 
 
 @section('content')
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2>  </h2>
-            </div>
-            <div class="pull-right">
-                <a class="btn btn-primary" href="" title="Go back"> <i class="fas fa-backward "></i> </a>
-            </div>
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2> </h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="/products" title="Go back"> <i class="fas fa-backward "></i> </a>
         </div>
     </div>
+</div>
 
-    <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Name:</strong>
+<div class="row">
 
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Description</strong>
-
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Price</strong>
-
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Date Created</strong>
-
-            </div>
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Название фильма</strong>
+            {{$product -> title}}
         </div>
     </div>
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Аннотация</strong>
+            {{$product -> content}}
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Дата</strong>
+            {{$product -> created_at}}
+        </div>
+    </div>
+</div>
 @endsection
