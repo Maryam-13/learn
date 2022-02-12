d00c1d8fb64c26f3ca93d3d103959213b978c23e
Maryam-13 <mary_am79@mail.ru>
2022-02-12 13:57:11 +0300

    Update create.blade.php

--- blog/resources/views/products/create.blade.php
+++ blog/resources/views/products/create.blade.php
@@ -4,10 +4,10 @@
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2>Add New Product</h2>
+                <h2>Добавить новую запись</h2>
             </div>
             <div class="pull-right">
-                <a class="btn btn-primary" href="" title="Go back"> <i class="fas fa-backward "></i> </a>
+                <a class="btn btn-primary" href="/products" title="Go back"> <i class="fas fa-backward "></i> </a>
             </div>
         </div>
     </div>
@@ -28,25 +28,20 @@
         <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
-                    <strong>Name:</strong>
-                    <input type="text" name="name" class="form-control" placeholder="Name">
+                    <strong>Название фильма</strong>
+                    <input type="text" name="title" class="form-control" placeholder="Навание фильма">
                 </div>
             </div>
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
-                    <strong>Description:</strong>
-                    <textarea class="form-control" style="height:50px" name="introduction"
-                        placeholder="description"></textarea>
+                    <strong>Аннотация</strong>
+                    <textarea class="form-control" style="height:50px" name="content"
+                        placeholder="Аннотация"></textarea>
                 </div>
             </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Price:</strong>
-                    <input type="number" name="price" class="form-control" placeholder="Put the price">
-                </div>
-            </div>
+           
             <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary">Добавить</button>
             </div>
         </div>
 
