771c5cf30ce764afb82de1f083da19dda9fe9fa6
Maryam-13 <mary_am79@mail.ru>
2022-04-21 23:10:33 +0300

    Update Index.vue

--- library/resources/js/Pages/Book/Index.vue
+++ library/resources/js/Pages/Book/Index.vue
@@ -2,57 +2,31 @@
   <Head title="Dashboard" />
 
   <BreezeAuthenticatedLayout>
-    <template #header>
-      <h2 class="text-xl font-semibold leading-tight text-gray-800">Book</h2>
-    </template>
+    <!-- <template #header>
+      <h2 class="text-xl font-semibold leading-tight text-gray-800">Библиотека</h2>
+    </template>-->
 
     <div class="py-12">
       <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
           <div class="p-6 bg-white border-b border-gray-200">
-            <div class="mb-4">
-              <Link
-                class="px-6 py-2 mb-2 text-green-100 bg-green-500 rounded"
-                :href="route('books.create')"
-              >
-                Добавить книгу
-              </Link>
-
-              <Link
-                class="px-6 py-2 mb-2 text-green-100 bg-green-500 rounded"
-                :href="route('books.show')"
-              >
-                Мои книги
-              </Link>
+            <div class="menu">
+              <Link :href="route('books.index')" class="link"> Все книги </Link>
+              <Link :href="route('books.show')" class="link"> Мои книги </Link>
+              <Link :href="route('books.issued')" class="link"> Выдано </Link>
             </div>
-            <table>
-              <thead class="font-bold bg-gray-300 border-b-2">
-                <td class="px-4 py-2">ID</td>
-                <td class="px-4 py-2">Название книги</td>
-                <td class="px-4 py-2">Автор</td>
-                <td class="px-4 py-2">Изображение</td>
-                <td class="px-4 py-2">Аннотация</td>
-                <td class="px-4 py-2">Действие</td>
-              </thead>
-              <tbody>
-                <tr v-for="book in books.data" :key="book.id">
-                  <td class="px-4 py-2">{{ book.id }}</td>
-                  <td class="px-4 py-2">{{ book.title }}</td>
-                  <td class="px-4 py-2">{{ book.author }}</td>
-                  <td class="px-4 py-2">
-                    <img :src="showImage() + book.image" class="object-cover h-70 w-40" />
-                  </td>
-                  <td class="px-4 py-2">{{ book.annotation }}</td>
-                  <td class="px-4 py-2 font-extrabold">
-                    <Link class="text-green-700" :href="route('books.edit', book.id)">
-                      Edit
-                    </Link>
-                    <Link @click="destroy(book.id)" class="text-red-700">>Delete</Link>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
 
+            <div class="show_books" v-for="book in books.data" :key="book.id">
+              <div class="image">
+                <img :src="showImage() + book.image" />
+              </div>
+
+              <div class="info">
+                <div class="px-4 py-2">{{ book.title }}</div>
+                <div class="px-4 py-2">{{ book.author }}</div>
+                <div class="px-4 py-2">{{ book.annotation }}</div>
+              </div>
+            </div>
             <pagination :links="books.links" />
           </div>
         </div>
