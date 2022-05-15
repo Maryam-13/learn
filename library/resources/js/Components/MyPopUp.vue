36966cb0ee627d13c949ae064898903b61855278
Maryam-13 <mary_am79@mail.ru>
2022-05-15 15:31:36 +0300

    Update MyPopUp.vue

--- library/resources/js/Components/MyPopUp.vue
+++ library/resources/js/Components/MyPopUp.vue
@@ -1,36 +1,38 @@
-<template >
-   <div class="py-12">
-     
-  <div class="black" v-bind:class="{ visible: isActive }" ></div>
-  <div class="Pop-Up" v-bind:class="{ visible: isActive }">
-    <form @submit.prevent="checkBook(number)">
-      <h2 class="margin-bottom-20">Выдать книгу</h2>
-      <label for="title" class="margin-bottom-20">Кому:</label>
-      <div>
-        <input
-          type="text"
-          v-model="form.whom"
-          
-          class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
-        />
-        <!--<div v-if="form.errors.whom" class="font-bold text-red-600">
+<template>
+  <div class="py-12">
+    <div class="black" v-bind:class="{ visible: isActive }"></div>
+    <div class="Pop-Up" v-bind:class="{ visible: isActive }">
+      <form @submit.prevent="checkBook(number)">
+        <h2 class="margin-bottom-20">Выдать книгу</h2>
+        <label for="title" class="margin-bottom-20">Кому:</label>
+        <div>
+          <input
+            type="text"
+            v-model="form.whom"
+            class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
+          />
+          <!--<div v-if="form.errors.whom" class="font-bold text-red-600">
                     Поле Кому должно быть заполнено
                   </div>-->
-   
-        <label class="font-bold text-red-600">{{ texterr }}</label>
-      </div>
-      <div class="flex items-center mt-4 block-align-center">
-        <button class="px-6 py-2 text-white bg-green-500 rounded">Выдать</button>
 
-        <button
-          class="px-6 py-2 text-white bg-green-500 rounded margin-left-15"
-          @click.prevent="$emit('chancel')"
-        >
-          Отмена
-        </button>
-      </div>
-    </form>
-  </div>
+          <label class="font-bold text-red-600">{{ texterr }}</label>
+        </div>
+        <div class="flex items-center mt-4 block-align-center">
+          <button
+            class="px-6 py-2 text-white bg-green-500 rounded" @click="if(form.whom !== null) {$emit('chancel')};"
+          >
+            Выдать
+          </button>
+
+          <button
+            class="px-6 py-2 text-white bg-green-500 rounded margin-left-15"
+            @click.prevent="$emit('chancel')"
+          >
+            Отмена
+          </button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -51,36 +53,31 @@ export default {
     Link,
     Pagination,
   },
-  props:   {
+  props: {
     books: Object,
     errors: Object,
-    isActive:{
-      type: Boolean,
-      value: true
-    },
+    isActive: Boolean,
     number: Number,
-    
-    
   },
-setup() {
-  const form = useForm({
+  setup() {
+    const form = useForm({
       whom: null,
     });
     const texterr = ref(null);
-  return{
-    form,
-    texterr,
-    
-    checkBook(id) {
-       if (form.whom !== null) {
+    return {
+      form,
+      texterr,
+
+      checkBook(id) {
+        if (form.whom !== null) {
           form.post(route("books.chek", id));
-          isActive.value = !isActive.value;
+          Inertia.get(route('books.mybook'));
         } else {
           texterr.value = "Требуется указать Кому";
         }
       },
-  };
- /*   const form = useForm({
+    };
+    /*   const form = useForm({
       whom: null,
     });
    const isActive =  ref(false);
@@ -106,15 +103,15 @@ setup() {
       destroy(id) {
         Inertia.delete(route("books.destroy", { id }));
       },*/
-      /*ppvisible(id) {
+    /*ppvisible(id) {
         number.value = id;
         isActive.value = !isActive.value;
       },*/
-      /*
+    /*
       chancel() {
        isActive.value = !isActive.value;
       },*/
-   // };
+    // };
   },
 };
 </script>
